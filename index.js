5ee0e6b869b167112b5788ee7696d346f9ca394a
harneeetah <harneeetah@gmail.com>
2023-09-19 23:33:19 +0100

    calculator app

--- index.js
+++ index.js
@@ -1,50 +1,21 @@
-let display = document.getElementById('display')
-let currentInput = document.getElementById('currentInput')
-let mathOperator = document.getElementById('mathOperator')
-let preValue = 0
-
-function InputDisplay(value){
-    currentInput += value
-    display.innerText = currentInput
+// This function clears all the values
+function clearScreen() {
+    document.getElementById("result").value = "";
 }
-
-function removeDisplay(){
-    currentInput = ''
-    mathOperator = ''
-    preValue = 0
-    display.innerText = '0'
+ 
+// This function displays the values
+function display(value) {
+    document.getElementById("result").value += value;
 }
-
-function resultCalculation(){
-    if(currentInput === '') return;
-    const currentValue = parseFloat(currentInput)
-
-    switch (mathOperator){
-        case '+':
-            preValue += currentValue;
-        break;
-        case '-':
-            preValue -= currentValue;
-        break;
-        case '*':
-            preValue *= currentValue;
-        case '/':
-            if(currentValue !== 0){
-                preValue /= currentValue;
-            }else{
-                alert('Error : Division by zero');
-                clearDisplay();
-                return;
-            }
-        break;
-    default:
-        preValue = currentValue ;
-        break;                
-    }
-
-    display.innerText = preValue;
-    currentInput = '';
-    mathOperator = '';
+ 
+// This function evaluates the expression and returns the result
+function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
 }
-
-clearDisplay();
+function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
